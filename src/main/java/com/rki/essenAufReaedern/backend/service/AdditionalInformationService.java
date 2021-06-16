f28e0ee59c5a18347e02e8ad68477c2756f8f849
Arthur <ar.waldner@mci4me.at>
2021-06-16 20:17:49 +0200

    merge

--- src/main/java/com/rki/essenAufReaedern/backend/service/AdditionalInformationService.java
+++ src/main/java/com/rki/essenAufReaedern/backend/service/AdditionalInformationService.java
@@ -1,7 +1,11 @@
 package com.rki.essenAufReaedern.backend.service;
 
 import com.rki.essenAufReaedern.backend.entity.AdditionalInformation;
+import com.rki.essenAufReaedern.backend.repository.AdditionalInformationRepository;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -9,8 +13,16 @@ import java.util.logging.Logger;
  * @author arthurwaldner
  * The service class for the additional_information database table.
  */
-
+@Service
 public class AdditionalInformationService {
     private static final Logger LOGGER = Logger.getLogger(AdditionalInformationService.class.getName());
-    private AdditionalInformation additionalInformation;
+    private AdditionalInformationRepository additionalInformationRepository;
+
+    public List<AdditionalInformation> findAll() {
+        return additionalInformationRepository.findAll();
+    }
+    public List<AdditionalInformation> findByPersonId(Long personId) {
+        return additionalInformationRepository.findByPersonId(personId);
+    }
+
 }
