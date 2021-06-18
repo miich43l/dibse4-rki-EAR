b99aec7c45ccff713bda60ba595e481d3baa3cab
Arthur <ar.waldner@mci4me.at>
2021-06-18 21:42:11 +0200

    add merge branch

--- src/main/java/com/rki/essenAufRaedern/tsp/solver/ITspSolver.java
+++
@@ -1,8 +0,0 @@
-package com.rki.essenAufRaedern.tsp.solver;
-
-import com.rki.essenAufRaedern.tsp.util.AdjacencyMatrix;
-import com.rki.essenAufRaedern.tsp.util.TspPathSequence;
-
-public interface ITspSolver {
-    TspPathSequence solve(AdjacencyMatrix matrix, int startLocation);
-}
