3176296ba3808066f765a2e28b057b850815379d
Thomas Widmann <mail@thomas-widmann.at>
2021-06-20 22:58:44 +0200

    cleanup

--- src/main/java/com/rki/essenAufRaedern/ui/components/address/AddressEditorComponent.java
+++ src/main/java/com/rki/essenAufRaedern/ui/components/address/AddressEditorComponent.java
@@ -1,22 +1,11 @@
 package com.rki.essenAufRaedern.ui.components.address;
 
-import ch.qos.logback.core.Layout;
 import com.rki.essenAufRaedern.backend.entity.Address;
-import com.rki.essenAufRaedern.backend.entity.Order;
-import com.rki.essenAufRaedern.ui.components.orders.OrderDeliveryWidget;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.ComponentEvent;
-import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.charts.model.VerticalAlign;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
-import com.vaadin.flow.shared.Registration;
 
 public class AddressEditorComponent extends FormLayout {
 
