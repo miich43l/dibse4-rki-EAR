3555cb83260fdef26f347304ed2c1eed0e30db89
Thomas Widmann <mail@thomas-widmann.at>
2021-06-20 19:08:08 +0200

    first impl of test data loader

--- src/main/java/com/rki/essenAufRaedern/backend/repository/AddressRepository.java
+++ src/main/java/com/rki/essenAufRaedern/backend/repository/AddressRepository.java
@@ -2,6 +2,7 @@ package com.rki.essenAufRaedern.backend.repository;
 
 import com.rki.essenAufRaedern.backend.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
