e1f6ca233de78634934f1262e531e50cc81beb39
Thomas Widmann <mail@thomas-widmann.at>
2021-07-12 18:30:59 +0200

    created additional info test

---
+++ src/test/java/com/rki/essenAufRaedern/backend/service/AdditionalInformationServiceTest.java
@@ -0,0 +1,36 @@
+package com.rki.essenAufRaedern.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdditionalInformationServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByPersonId() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findForPerson() {
+    }
+
+    @Test
+    void findForPersonAndType() {
+    }
+}
